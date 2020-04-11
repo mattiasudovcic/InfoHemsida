e50d610980d7f8e15d1165e88bca401297e5f6e8
dendoshe <diana.jumaili@gmail.com>
2020-04-11 17:51:42 +0200

    Merge origin/devDiana into devDiana

--- src/informatikhemsida/InformatikHemsida.java
+++ src/informatikhemsida/InformatikHemsida.java
@@ -5,17 +5,10 @@
  */
 package informatikhemsida;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class InformatikHemsida {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args)  {
 
         
     }
