be506fa6fb3b75aa49c3dfcd342c5f686f69b506
dendoshe <diana.jumaili@gmail.com>
2020-04-11 17:33:16 +0200

    Kortat ner inloggmetod

--- src/informatikhemsida/InformatikHemsida.java
+++ src/informatikhemsida/InformatikHemsida.java
@@ -17,26 +17,6 @@ public class InformatikHemsida {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        /*detta vet jag inte vad det är*/
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-
-        /*detta är vad som upprättar en connection. Om det blir en error med något med TCP/IP, fråga Diana*/
-        String connectionURL = "jdbc:sqlserver://localhost:53158;databaseName=Informatik;user=admin;password=team15";
-        
-        /*Detta är en variabel för att komma åt data via connection*/
-        Connection con = DriverManager.getConnection(connectionURL);
-      
-        
-        System.out.println("Succé!");
-        
-        /*testa att det fungerar*/
-        Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery("Select * from Konto");
-        while (rs.next()){
-        int ID = rs.getInt(1);
-        String Name = rs.getString(2);
-        System.out.println(ID + " " + Name);}
-        
         
     }
     
